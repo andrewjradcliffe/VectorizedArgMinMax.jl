9e9f7cef6f166ef8329d5e1dede04869dac44923
Andrew Radcliffe <andrewjradcliffe@gmail.com>
2022-03-10 12:21:21 -0800

    Begin updates to logsumexp

--- src/lvlogsumexp.jl
+++ src/lvlogsumexp.jl
@@ -5,54 +5,68 @@
 #
 ############################################################################################
 
-function expminusbody(N::Int, D)
+function aminusb_exp_sumbody(N::Int, D)
     body = Expr(:block)
     params = D.parameters
     a = Expr(:ref, :A, ntuple(d -> Symbol(:i_, d), N)...)
     b = Expr(:ref, :B, ntuple(d -> params[d] == Static.One ? 1 : Symbol(:i_, d), N)...)
-    m = Expr(:ref, :α, ntuple(d -> params[d] == Static.One ? 1 : Symbol(:i_, d), N)...)
-    e = Expr(:(=), b, Expr(:call, :+, b, Expr(:call, :exp, Expr(:call, :-, a, m))))
+    c = Expr(:ref, :C, ntuple(d -> params[d] == Static.One ? 1 : Symbol(:i_, d), N)...)
+    e = Expr(:(=), c, Expr(:call, :+, c, Expr(:call, :exp, Expr(:call, :-, a, b))))
     push!(body.args, e)
     body
 end
 
-function expminus_quote(N::Int, D)
+function aminusb_exp_sum_quote(N::Int, D)
     ls = loopgen(N)
-    body = expminusbody(N, D)
+    body = aminusb_exp_sumbody(N, D)
     push!(ls.args, body)
     return quote
         @turbo $ls
-        return B
+        return C
     end
 end
 
-@generated function expminus!(B::AbstractArray{T, N}, A::AbstractArray{T, N}, α::AbstractArray{T, N}, dims::D) where {T, N, D}
-    expminus_quote(N, D)
+@generated function aminusb_exp_sum!(C::AbstractArray{T, N}, A::AbstractArray{T, N}, B::AbstractArray{T, N}, dims::D) where {T, N, D}
+    aminusb_exp_sum_quote(N, D)
 end
 
-function apluslogb!(A::AbstractArray{T, N}, B::AbstractArray{T, N}) where {T, N}
+# function apluslogb!(A::AbstractArray{T, N}, B::AbstractArray{T, N}) where {T, N}
+#     @turbo for i ∈ eachindex(A)
+#         A[i] += log(B[i])
+#     end
+#     A
+# end
+# function apluslogb!(C::AbstractArray{T, N}, A::AbstractArray{T, N}, B::AbstractArray{T, N}) where {T, N}
+#     @turbo for i ∈ eachindex(A)
+#         C[i] = A[i] + log(B[i])
+#     end
+#     C
+# end
+# apluslogb(A::AbstractArray{T, N}, B::AbstractArray{S, N}) where {T, N, S} =
+#     apluslogb!(similar(A, promote_type(T, S)), A, B)
+
+function logself_plusb!(C::AbstractArray{T, N}, B::AbstractArray{T, N}) where {T, N}
     @turbo for i ∈ eachindex(A)
-        A[i] += log(B[i])
-    end
-    A
-end
-function apluslogb!(C::AbstractArray{T, N}, A::AbstractArray{T, N}, B::AbstractArray{T, N}) where {T, N}
-    @turbo for i ∈ eachindex(A)
-        C[i] = A[i] + log(B[i])
+        C[i] = B[i] + log(C[i])
     end
     C
 end
-apluslogb(A::AbstractArray{T, N}, B::AbstractArray{S, N}) where {T, N, S} =
-    apluslogb!(similar(A, promote_type(T, S)), A, B)
 
 function lvlse(A::AbstractArray{T, N}, dims::NTuple{M, Int}) where {T, N, M}
-    α = lvmaximum(A, dims=dims)
-    Dᴮ = size(α)
-    Tₒ = Base.promote_op(exp, T)
-    B = zeros(Tₒ, Dᴮ)
+    # α = lvmaximum(A, dims=dims)
+    # Dᴮ = size(α)
+    # Tₒ = Base.promote_op(exp, T)
+    # B = zeros(Tₒ, Dᴮ)
+    # Dᴮ′ = ntuple(d -> d ∈ dims ? StaticInt(1) : Dᴮ[d], N)
+    # expminus!(B, A, α, Dᴮ′)
+    # return eltype(α) <: AbstractFloat ? apluslogb!(α, B) : apluslog(α, B)
+    B = lvmaximum(A, dims=dims)
+    Dᴮ = size(B)
     Dᴮ′ = ntuple(d -> d ∈ dims ? StaticInt(1) : Dᴮ[d], N)
-    expminus!(B, A, α, Dᴮ′)
-    return eltype(α) <: AbstractFloat ? apluslogb!(α, B) : apluslog(α, B)
+    C = zeros(Base.promote_op(exp, T), Dᴮ)
+    aminusb_exp_sum!(C, A, B, Dᴮ′)
+    logself_plusb!(C, B)
+    C
 end
 
 lvlse(A::AbstractArray{T, N}, dims::Int) where {T, N} = lvlse(A, (dims,))
