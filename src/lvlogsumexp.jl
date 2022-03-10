64e42ca0d4e8b2984672f8cb499e0e6af07f6137
Andrew Radcliffe <andrewjradcliffe@gmail.com>
2022-03-09 18:19:02 -0800

    Add threaded logsumexp

--- src/lvlogsumexp.jl
+++ src/lvlogsumexp.jl
@@ -42,7 +42,8 @@ function apluslogb!(C::AbstractArray{T, N}, A::AbstractArray{T, N}, B::AbstractA
     end
     C
 end
-apluslogb(A::AbstractArray{T, N}, B::AbstractArray{S, N}) = apluslogb!(similar(A, promote_type(T, S)), A, B)
+apluslogb(A::AbstractArray{T, N}, B::AbstractArray{S, N}) where {T, N, S} =
+    apluslogb!(similar(A, promote_type(T, S)), A, B)
 
 function lvlse(A::AbstractArray{T, N}, dims::NTuple{M, Int}) where {T, N, M}
     α = lvmaximum(A, dims=dims)
@@ -72,3 +73,60 @@ function lvlse1(A::AbstractArray{T, N}) where {T, N}
     α + log(s)
 end
 
+################ threaded version
+function texpminus_quote(N::Int, D)
+    ls = loopgen(N)
+    body = expminusbody(N, D)
+    push!(ls.args, body)
+    return quote
+        @tturbo $ls
+        return B
+    end
+end
+
+@generated function texpminus!(B::AbstractArray{T, N}, A::AbstractArray{T, N}, α::AbstractArray{T, N}, dims::D) where {T, N, D}
+    texpminus_quote(N, D)
+end
+
+function tapluslogb!(A::AbstractArray{T, N}, B::AbstractArray{T, N}) where {T, N}
+    @tturbo for i ∈ eachindex(A)
+        A[i] += log(B[i])
+    end
+    A
+end
+function tapluslogb!(C::AbstractArray{T, N}, A::AbstractArray{T, N}, B::AbstractArray{T, N}) where {T, N}
+    @tturbo for i ∈ eachindex(A)
+        C[i] = A[i] + log(B[i])
+    end
+    C
+end
+tapluslogb(A::AbstractArray{T, N}, B::AbstractArray{S, N}) where {T, N, S} =
+    tapluslogb!(similar(A, promote_type(T, S)), A, B)
+
+function lvtlse(A::AbstractArray{T, N}, dims::NTuple{M, Int}) where {T, N, M}
+    α = lvtmaximum(A, dims=dims)
+    Dᴮ = size(α)
+    Tₒ = Base.promote_op(exp, T)
+    B = zeros(Tₒ, Dᴮ)
+    Dᴮ′ = ntuple(d -> d ∈ dims ? StaticInt(1) : Dᴮ[d], N)
+    texpminus!(B, A, α, Dᴮ′)
+    tapluslogb!(α, B)
+    return eltype(α) <: AbstractFloat ? tapluslogb!(α, B) : tapluslog(α, B)
+end
+
+lvtlse(A::AbstractArray{T, N}, dims::Int) where {T, N} = lvtlse(A, (dims,))
+lvtlse(A::AbstractArray{T, N}; dims=:) where {T, N} = lvtlse(A, dims)
+lvtlse(A::AbstractArray{T, N}) where {T, N} = lvtlse1(A)
+lvtlse(A::AbstractArray{T, N}, ::Colon) where {T, N} = lvtlse1(A)
+
+function lvtlse1(A::AbstractArray{T, N}) where {T, N}
+    α = typemin(T)
+    s = zero(promote_type(T, Float64))
+    @tturbo for i ∈ eachindex(A)
+        α = max(A[i], α)
+    end
+    @tturbo for i ∈ eachindex(A)
+        s += exp(A[i] - α)
+    end
+    α + log(s)
+end
